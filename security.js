d4a7791fb8d204843f4cc4caf02d6431bdcff102
Amir Malik <a@unoc.net>
2011-03-05 19:35:37 -0800

    Add basic encryption and decryption support (salting).

---
+++ security.js
@@ -0,0 +1,55 @@
+/**
+ * security.js
+ * Security routines
+ *
+ * @author Amir Malik
+ */
+
+var crypto = require('crypto');
+
+function SaltService() {
+  this.key = null;
+  this.fields = {};
+}
+
+SaltService.prototype.init = function(config) {
+  this.algorithm = config['algorithm'] || 'aes192';
+  this.key = config['key']['string'];
+  this.fields = config['fields'];
+};
+
+SaltService.prototype._docrypt = function(collection, doc, encrypt) {
+  var keys = this.fields[collection];
+
+  var algo = this.algorithm;
+  var key = this.key;
+
+  if(keys) {
+    keys.forEach(function(key) {
+      var c;
+
+      if(encrypt) {
+        c = crypto.createCipher(algo, key);
+      } else {
+        c = crypto.createDecipher(algo, key);
+      }
+
+      c.update(doc[key]);
+      doc[key] = c.final();
+    });
+
+    return doc;
+  } else {
+    return doc;
+  }
+};
+
+SaltService.prototype.encrypt = function(collection, doc) {
+  return this._docrypt(collection, doc, true);
+};
+
+SaltService.prototype.decrypt = function(collection, doc) {
+  return this._docrypt(collection, doc, false);
+};
+
+exports.SaltService = global['SaltService'] || (global['SaltService'] = new SaltService());
---
+++ test/security.js
@@ -0,0 +1,24 @@
+var SaltService = require('../security').SaltService;
+SaltService.init({
+  algorithm: "aes192",
+  key: {
+    string: "secret"
+  },
+  fields: {
+    "c1": ["foo", "bar"]
+  }
+});
+
+var doc = {
+  foo: "the real foo",
+  bar: "the real bar"
+};
+console.log("encrypting: " + JSON.stringify(doc, null, 2));
+
+var doc2 = SaltService.encrypt('c1', doc);
+
+console.log("encrypted: " + JSON.stringify(doc2, null, 2));
+
+var doc3 = SaltService.decrypt('c1', doc2);
+
+console.log("decrypted: " + JSON.stringify(doc3, null, 2));
